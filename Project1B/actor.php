d11dcea01b12c55be6271a1cba9f82f5acade706
yifanshu <466763052@qq.com>
2019-02-08 13:07:22 -0800

    Finsh the actor page

---
+++ Project1B/actor.php
@@ -0,0 +1,96 @@
+<html>
+
+<body>
+<?php
+
+$actor = $_GET["actor"];
+$servername = "localhost";
+$username = "cs143";
+$password = "";
+
+if($actor) {
+	$conn = mysql_connect($servername, $username, $password);
+	if (!$conn) {
+		echo "Unable to connect to the sql server.";
+		die();
+	}
+
+	if (!mysql_select_db("CS143", $conn)){
+		echo "Unable to select the data base 'CS143'";
+		die();
+	}
+	
+	$query = sprintf("SELECT id, CONCAT(first, ' ', last) AS name, sex, dob, dod
+					  FROM Actor
+					  WHERE LOWER(CONCAT(first, ' ', last)) = LOWER('%s')",
+		     		  $actor);
+	$output = mysql_query($query, $conn);
+	displayResult($output, "movie", false, 1);
+
+	$query = 	$query = sprintf("SELECT title, year, shownMovie.name, shownMovie.role, rating, company
+								  FROM Movie,
+								  (
+								  	SELECT mid, target.name, role
+								  	FROM MovieActor,
+								  	(
+								  		SELECT id, CONCAT(first, ' ', last) AS name
+								  		FROM Actor 
+								  		WHERE LOWER(CONCAT(first, ' ', last)) = LOWER('%s')
+								  	) AS target
+								  	WHERE target.id = MovieActor.aid
+								  ) AS shownMovie
+								  WHERE Movie.id = shownMovie.mid
+								  ORDER BY year",
+								  $actor);
+
+	$output = mysql_query($query, $conn);
+	displayResult($output, "actor", true, 0);
+
+
+}
+
+
+function displayResult($data1, $type, $link, $linkColumn) {
+    if (mysql_num_rows($data1) > 0) {
+        echo "<table border=1  <tr>";
+        for ($i=0; $i < mysql_num_fields($data1); $i++){
+            echo "<th>";
+            echo mysql_fetch_field($data1)->name;
+            echo "</th>";
+		}
+        echo "</tr>";
+        while ($row = mysql_fetch_row($data1)) {
+            echo "<tr>";
+            for ($j = 0; $j < mysql_num_fields($data1); $j ++) {
+                echo "<td>";
+				if($j == $linkColumn) {
+					if($link) {
+						echo "<a href= '". $type . ".php" . "?" . $type . "=" . $row[$j] . "'>";
+						echo $row[$j];
+						echo "</a>";
+					} else {
+						echo $row[$j];
+					}
+				} else {
+					if ($row[$j] == null) {
+						echo "NULL";
+					} else {
+						echo $row[$j];
+					}
+				}
+                echo "</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
+    else {
+        echo "The return data is empty";
+    }
+	mysql_free_result($data1);
+}
+
+?>
+</body>
+
+</html>
