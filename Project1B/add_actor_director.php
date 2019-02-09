4041ff0c04548dc4b9960d09575f3a9a14381bc6
chengshunzhang <chengshun.zhang@outlook.com>
2019-02-08 21:05:44 -0800

    complete add functionalities

--- Project1B/add_actor_director.php
+++ Project1B/add_actor_director.php
@@ -9,6 +9,7 @@
 			<a href="add_movie.php"> Add a New Movie </a>
 			<a href="add_comments.php"> Add New Comments </a>
 			<a href="add_movieactor.php"> Add a New Actor to a Movie </a>
+			<a href="add_moviedirector.php"> Add a New Director to a Movie </a>
 			<a href="search.php"> Search </a>
 		</div>
 		<h3> Adding a new director or actor to the database: </h3>
--- Project1B/add_moviedirector.php
+++ Project1B/add_moviedirector.php
@@ -3,7 +3,7 @@
 		<link rel="stylesheet" type="text/css" href="main.css">
 	</head>
 	<body>
-		<h1> Add New Actor to a movie! </h1>
+		<h1> Add New Director to a movie! </h1>
 		<div class="nav">
 			<a href="add_actor_director.php"> Add Actor/Director </a>
 			<a href="add_movie.php"> Add a New Movie </a>
@@ -12,8 +12,8 @@
 			<a href="add_moviedirector.php"> Add a New Director to a Movie </a>
 			<a href="search.php"> Search </a>
 		</div>
-		<h3> Adding a new actor to the movie: </h3>
-		<form action="add_movieactor.php" method="GET">
+		<h3> Adding a new director to the movie: </h3>
+		<form action="add_moviedirector.php" method="GET">
 		    FirstName: <input type="text" name="Firstname" placeholder="firstname" size=20 maxlength=20>
 		    <br>
 		    LastName: <input type="text" name="Lastname" placeholder="lastname" size=20 maxlength=20>
@@ -47,8 +47,6 @@
 		    	       
 		    	   ?>
 		    <br>
-		    Role in the movie: <input type="text" name="Role" size=20 maxlength=50>
-		    <br>
 		    <input type="submit" name="add" value="Submit">
 		</form>
 	</body>
@@ -71,22 +69,21 @@
 			
 			$sanitized_firstname = mysql_real_escape_string($_GET["Firstname"], $db_connection);
 			$sanitized_lastname = mysql_real_escape_string($_GET["Lastname"], $db_connection);
-			$query = "select id from Actor where last = '$sanitized_lastname' and
+			$query = "select id from Director where last = '$sanitized_lastname' and
 					first = '$sanitized_firstname'";
 			$rs = mysql_query($query, $db_connection);
 			if($rs) {
-				$aid = mysql_fetch_row($rs)[0];
+				$did = mysql_fetch_row($rs)[0];
 				$moviename = $_GET["Movie"];
 				$query = "select id from Movie where title = '$moviename'";
 				$mid = mysql_fetch_row(mysql_query($query, $db_connection))[0];
-				$sanitized_role = mysql_real_escape_string($_GET["Role"], $db_connection);
-				$query = "insert into MovieActor (mid, aid, role) values
-						($mid, $aid, '$sanitized_role')";
+				$query = "insert into MovieDirector (mid, aid) values
+						($mid, $aid)";
 				$rs = mysql_query($query, $db_connection);
 				if($rs) {
-					echo "New actor in a movie successfully added!";
+					echo "New director in a movie successfully added!";
 				} else {
-					echo "The actor not found! Please add the actor to database first!";
+					echo "The director not found! Please add the director to database first!";
 				}
 			} else {
 				echo "Insertion failed!";
